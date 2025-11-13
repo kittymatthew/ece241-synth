fbe7607c4efb125ea3f1c7101617fdd73278af99
Kittymatthew <matthew.gibson02@hotmail.com>
2025-11-13 16:33:56 -0500

    final fix

--- src/synth.v
+++ src/synth.v
@@ -51,6 +51,7 @@ module synth (
     wire sent_enable;
     wire [7:0]keyboard_input;
     wire unused;
+    wire unused2;
 
     // Static assignments
     assign reset = !KEY[0];
@@ -64,6 +65,6 @@ module synth (
     Audio_Controller processor  (CLOCK_50, reset, 1'b0, 1'b0, 1'b0, AUDIO_OUT, AUDIO_OUT, // Output the same signal to the left and right channels
                                 enable, AUD_ADCDAT, AUD_BCLK, AUD_ADCLRCK, AUD_DACLRCK, left_unused, // Audio inputs are not used
                                 right_unused, audio_in_unused, audio_out_allowed, AUD_XCK, AUD_DACDAT);
-    PS2_Controller keyboard(CLOCK_50, reset, 8'b0, 1'b0, PS2_CLK, PS2_DAT, unused, unused, keyboard_input, sent_enable); 
-    keyboard_transfer decoder(CLOCK_50, reset, keyboard_input, sent_enable, NOTE);
+    PS2_Controller keyboard(CLOCK_50, reset, 8'b0, 1'b0, PS2_CLK, PS2_DAT, unused, unused2, keyboard_input, sent_enable); 
+    keyboard_transfer decoder(CLOCK_50, reset, keyboard_input, 1'b1, NOTE);
 endmodule
